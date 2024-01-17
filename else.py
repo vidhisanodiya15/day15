8f7840817d30fc77b476ebd79de4ba0114320fc2
Vidhi sanodiya <114800338+vidhisanodiya15@users.noreply.github.com>
2024-01-17 22:30:39 +0530

    Add files via upload

---
+++ else.py
@@ -0,0 +1,22 @@
+# a=34
+# b=54
+# if b>a:
+#     print("b is greater than a")
+# elif a==b:
+#     print("a and b are equal")
+# else:
+#     print("a is greater than b")
+
+# a = 2
+# b = 330
+# print("A") if a > b else print("B")
+
+# a = 330
+# b = 330
+# print("A") if a > b else print("=") if a == b else print("B")
+
+a = 200
+b = 33
+c = 500
+if a > b and c > a:
+  print("Both conditions are True")
---
+++ if,else.py
@@ -0,0 +1,4 @@
+a=23
+b=54
+if b>a:
+    print("b is greater than a")
